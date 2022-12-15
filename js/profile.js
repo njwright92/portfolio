f40f4957342908e50d6ab33082f9da62a42ff19d
njwright92 <njwright92@gmail.com>
2022-12-14 20:20:18 -0800

    proofread

--- js/profile.js
+++ js/profile.js
@@ -13,14 +13,14 @@ function getSport()  {
     const target = "snowboarding";
     console.log(target);
         do  {
-            guess = prompt(`what is my favorite sport? you have 3 guesses`);
+            guess = prompt(`what is my favorite sport? Your options are \n\n ${favoriteSport.join(" , ")}`);
             numTries += 1;
             if (guess === null) {
                 alert("game aborted");
                 return
-            } correct = checkGuess(guess, target)
+            } correct = checkGuess(guess, target);
         } while (!correct)  {
-            alert(`Finally you chose wisely it took you ${numTries} to guess correctly! \n Scroll to bottom to chekcout prize`);
+            alert(`Finally you chose wisely.\n It took you ${numTries} to guess correctly! \n Scroll to bottom to checkout prize!`);
             showGif();
         }
 }
@@ -29,13 +29,13 @@ function checkGuess(guess)  {
     let correct = false;
     console.log(guess);
     if (!favoriteSport.includes(guess))  {
-        alert("your guess is not in the options")
+        alert("Your guess is not in the options.")
     } else if (guess === "baseball")  {
-        alert("incorrect guess again");
+        alert("incorIect guess again.");
     } else if (guess === "football")  {
-        alert("not even close dummy");
+        alert("Not even close dummy.");
     } else if (guess === "skateboarding")  {
-        alert("wrong guess again");
+        alert("wrong guess again.");
     } else {
         correct = true;
     } return correct;
