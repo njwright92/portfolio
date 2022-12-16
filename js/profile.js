4cad667547840254ff842f738797ed1e2dcb5676
njwright92 <njwright92@gmail.com>
2022-12-15 23:28:12 -0800

    cdns and js

--- js/profile.js
+++ js/profile.js
@@ -7,18 +7,17 @@ function showGif()  {
 const favoriteSport = ["baseball", "football", "skateboarding", "snowboarding"];
 favoriteSport.sort();
 function getSport()  {
-    let guess = "snowboarding";
+    let guess;
     let numTries = 0;
     let correct = false;
-    const target = "snowboarding";
-    console.log(target);
+    console.log();
         do  {
             guess = prompt(`what is my favorite sport? Your options are \n\n ${favoriteSport.join(" , ")}`);
             numTries += 1;
             if (guess === null) {
                 alert("game aborted");
                 return
-            } correct = checkGuess(guess, target);
+            } correct = checkGuess(guess.toLowerCase().trim());
         } while (!correct)  {
             alert(`Finally you chose wisely.\n It took you ${numTries} to guess correctly! \n Scroll to bottom to checkout prize!`);
             showGif();
@@ -29,7 +28,7 @@ function checkGuess(guess)  {
     let correct = false;
     console.log(guess);
     if (!favoriteSport.includes(guess))  {
-        alert("Your guess is not in the options.")
+        alert("Your guess is not one of the choices.")
     } else if (guess === "baseball")  {
         alert("incorIect guess again.");
     } else if (guess === "football")  {
