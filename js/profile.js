e513b984e7cf8048e1d88e3ec3080d0895c64e89
njwright92 <njwright92@gmail.com>
2022-12-19 23:50:23 -0800

    js

--- js/profile.js
+++ js/profile.js
@@ -1,44 +1,39 @@
 const game = document.querySelector('#about');
 game.addEventListener('click', getSport);
 
-
-function showGif()  {
-    const img = document.createElement("img");
-    img.src = "images/ripper-snowboarder.gif";
-    document.body.appendChild(img);
-}
-
 const favoriteSport = ["baseball", "football", "skateboarding", "snowboarding"];
 favoriteSport.sort();
 
-function getSport()  {
+function getSport() {
     let guess;
     let numTries = 0;
     let correct = false;
     console.log();
-        do  {
-            guess = prompt(`what is my favorite sport? Your options are \n\n ${favoriteSport.join(" , ")}`);
-            numTries += 1;
-            if (guess === null) {
-                alert("game aborted");
-                return
-            } correct = checkGuess(guess.toLowerCase().trim());
-        } while (!correct)  {
-            alert(`Finally you chose wisely.\n It took you ${numTries} to guess correctly! \n Scroll to bottom to checkout prize!`);
-            showGif();
-        }
+    do {
+        guess = prompt(`what is my favorite sport? Your options are \n\n ${favoriteSport.join(" , ")}`);
+        numTries += 1;
+        if (guess === null) {
+            alert("game aborted");
+            return
+        } correct = checkGuess(guess.toLowerCase().trim());
+    } while (!correct) {
+        const img = document.createElement("img");
+        img.src = "images/ripper-snowboarder.gif";
+        document.body.appendChild(img)
+        alert(`Finally you chose wisely.\n It took you ${numTries} to guess correctly! \n Scroll to bottom to checkout prize!`);
+    }
 }
 
-function checkGuess(guess)  {
+function checkGuess(guess) {
     let correct = false;
     console.log(guess);
-    if (!favoriteSport.includes(guess))  {
+    if (!favoriteSport.includes(guess)) {
         alert("Your guess is not one of the choices.")
-    } else if (guess === "baseball")  {
+    } else if (guess === "baseball") {
         alert("Incorrect guess again.");
-    } else if (guess === "football")  {
+    } else if (guess === "football") {
         alert("Not even close dummy.");
-    } else if (guess === "skateboarding")  {
+    } else if (guess === "skateboarding") {
         alert("wrong guess again.");
     } else {
         correct = true;
