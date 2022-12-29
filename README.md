86356fa651602169104b4ad4f939f086da88cca7
njwright92 <njwright92@gmail.com>
2022-12-28 23:44:42 -0800

    wtf

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-- 👋 Hi, Nathan Wright
-- 👀 I’m interested in developing new products/ideas, snowboarding, money and traveling.
-- 🌱 I’m currently learning full-stack development through NuCamp.
-- 💞️ I’m looking to collaborate on website/app ideas production tracking, recipe books, and comedy directories.
-- 📫 You can reach me via Email @ njwright92@gmail.com
-
-<!---
-njwright92/njwright92 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+# portfolio
---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+const game = document.querySelector('#about');
+game.addEventListener('click', getSport);
+
+const favoriteSport = ["baseball", "football", "skateboarding", "snowboarding"];
+favoriteSport.sort();
+
+function getSport() {
+    let guess;
+    let numTries = 0;
+    let correct = false;
+    console.log();
+    do {
+        guess = prompt(`what is my favorite sport? Your options are \n\n ${favoriteSport.join(", ")}`);
+        numTries += 1;
+        if (guess === null) {
+            alert("game aborted");
+            return
+        } correct = checkGuess(guess.toLowerCase().trim());
+    } while (!correct) {
+        const img = document.createElement("img");
+        img.src = "images/ripper-snowboarder.gif";
+        const gif_game = document.getElementById("gif_game");
+        gif_game.appendChild(img);
+        img.style.width = '20rem';
+        alert(`Winner! Snowboardings my favorite, it only took you ${numTries} tries!`);
+    }
+}
+
+
+
+function checkGuess(guess) {
+    let correct = false;
+    console.log(guess);
+    if (!favoriteSport.includes(guess)) {
+        alert("Your guess is not one of the choices.")
+    } else if (guess === "baseball") {
+        alert("Incorrect guess again.");
+    } else if (guess === "football") {
+        alert("Not even close dummy.");
+    } else if (guess === "skateboarding") {
+        alert("wrong guess again.");
+    } else {
+        correct = true;
+    } return correct;
+}
+
+$(function () {
+    $(".carousel").carousel({ interval: 4000 });
+});
