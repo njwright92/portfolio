3350b2e603f7b99f5b64731106baac6b08456190
njwright92 <njwright92@gmail.com>
2023-01-29 11:50:14 -0800

    interests

--- ReactVersion/portfolio/src/pages/Interests.js
+++ ReactVersion/portfolio/src/pages/Interests.js
@@ -10,6 +10,14 @@ const Interests = () => {
         <Col className="col-6 mx-auto img-fluid">
           <Carousels />
         </Col>
+        <Col className="col-6 mx-auto row-content">
+          <><h2>Interests</h2>
+            <h5>Snowboarding</h5>
+            <h5>I'm currently a Chef/Bartender, but I'm studying to transition into tech.</h5>
+            <h5>Traveling is great, I love new experiences, and want to see more cultures.</h5>
+            <h5>Also love money, and women.</h5>
+          </>
+        </Col>
       </Row>
     </Container >
   );
