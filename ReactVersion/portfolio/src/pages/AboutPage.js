ceb2f32fd7a3ee894bd2a9a11f094b3c92e820d0
njwright92 <njwright92@gmail.com>
2023-02-02 14:03:50 -0800

    responsiveness

--- ReactVersion/portfolio/src/pages/AboutPage.js
+++ ReactVersion/portfolio/src/pages/AboutPage.js
@@ -8,18 +8,16 @@ const About = () => {
       <SubHeader current='about' />
       <h1><strong>About Me</strong></h1>
       <Row className="align-items-center">
-        <Col className="col-7 col-md-6 col-lg-5 mx-auto img-fluid">
+        <Col className="col-7 col-md-6 col-lg-5 mx-auto">
           <Carousels2 />
         </Col>
         <Col className="col-5 col-md-6 col-lg-7 mx-auto row-content">
-          <>
-            <ul>
-              <h5>Snowboarding</h5>
-              <li><h5>I'm currently a Chef/Bartender, but I'm studying to transition into tech.</h5></li>
-              <li><h5>Traveling is great, I love new experiences, and want to see more cultures.</h5></li>
-              <li><h5>Also love money, and women.</h5></li>
-            </ul>
-          </>
+          <ul>
+            <h5>Snowboarding</h5>
+            <li><h5>I'm currently a Chef/Bartender, but I'm studying to transition into tech.</h5></li>
+            <li><h5>Traveling is great, I love new experiences, and want to see more cultures.</h5></li>
+            <li><h5>Also love money, and women.</h5></li>
+          </ul>
         </Col>
         <Col>
           <p>"I got love"<audio class="mt-1" src={require("../mp3s/i-got-love.mp3")} controls loop></audio></p>
