c5a54cab043a49e2b7480e9571aa0e6e36373238
Nathan Wright <njwright92@gmail.com>
2023-03-31 15:26:21 -0700

    Update AboutPage.js

--- ReactVersion/src/pages/AboutPage.js
+++ ReactVersion/src/pages/AboutPage.js
@@ -5,38 +5,33 @@ import SubHeader from "../components/SubHeader";
 const About = () => {
   return (
     <Container>
-      <SubHeader current='about' />
-      <h1><strong>About Me</strong></h1>
+      <SubHeader current="about" />
+      <h1 className="text-center mb-5"><strong>About Me</strong></h1>
       <Row className="align-items-center">
-        <Col className="col-7 col-md-6 col-lg-5 mx-auto">
+        <Col className="col-12 col-md-6 col-lg-5 mx-auto">
           <Carousels2 />
         </Col>
-        <Col className="col-5 col-md-6 col-lg-7 mx-auto row-content">
-          <ul>
-            <li>
-              <h5>I'm currently a Chef/Bartender, but I'm studying to transition into tech.
-              </h5>
+        <Col className="col-12 col-md-6 col-lg-7 mx-auto">
+          <ul className="list-unstyled">
+            <li className="mb-4">
+              <h5 className="font-weight-bold">I'm currently a Chef/Bartender, but I'm studying to transition into tech.</h5>
             </li>
-            <li>
-              <h5>I love traveling and discovering new cultures. It's a great way to gain new experiences and broaden my horizons
-              </h5>
+            <li className="mb-4">
+              <h5 className="font-weight-bold">I love traveling and discovering new cultures. It's a great way to gain new experiences and broaden my horizons.</h5>
             </li>
-            <li>
-              <h5>"Something you might not know by looking at me is I can run really fast"
-                <em>Jerry Seinfield'</em> Big fan of standup comedy.
-              </h5>
+            <li className="mb-4">
+              <h5 className="font-weight-bold">"Something you might not know by looking at me is I can run really fast" - <em>Jerry Seinfeld</em>. I'm a big fan of stand-up comedy.</h5>
             </li>
-            <li>
-              <h5>I enjoy the process of creation, whether it be brewing, cooking, or building web applications
-              </h5>
+            <li className="mb-4">
+              <h5 className="font-weight-bold">I enjoy the process of creation, whether it be brewing, cooking, or building web applications.</h5>
             </li>
           </ul>
         </Col>
-        <Col>
-          <p>"I got love"
-            <audio class="mt-1" src={require("../mp3s/i-got-love.mp3")} controls loop>
-            </audio>
-          </p>
+      </Row>
+      <Row className="justify-content-center mt-5">
+        <Col className="col-8 col-md-6 col-lg-4 text-center">
+          <p className="font-italic mb-0">"I got love"</p>
+          <audio className="mt-1" src={require("../mp3s/i-got-love.mp3")} controls loop></audio>
         </Col>
       </Row>
     </Container>
